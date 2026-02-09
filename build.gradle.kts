b510670ae4ea797f35f5fd3a8fe4ac9f61117761
Sebastian Cabrera <ccabreram1@est.ups.edu.ec>
2026-02-08 23:18:35 -0500

    JWT correccion

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,9 +32,9 @@ dependencies {
     runtimeOnly ("org.postgresql:postgresql")
     
     // JWT
-    implementation ("io.jsonwebtoken:jjwt-api:0.12.3")
-    runtimeOnly ("io.jsonwebtoken:jjwt-impl:0.12.3")
-    runtimeOnly ("io.jsonwebtoken:jjwt-jackson:0.12.3")
+    implementation("io.jsonwebtoken:jjwt-api:0.12.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
     
     // Jackson for Java 8+ dates
     implementation ("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
